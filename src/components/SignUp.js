ea98408813ec3bd55337197bf2b45177d40840dd
hhyukk <hhyukk1273@gmail.com>
2025-05-25 12:06:30 +0900

    refactor: form 추가

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -8,56 +8,59 @@ function SignUp() {
         <h1>내담자 회원가입</h1>
         <hr />
         <CgProfile className={styles.user_profile} />
-        <div>
-          <span>아이디</span>
-          <input />
-        </div>
-        <div>
-          <span>아이디</span>
-          <input />
-        </div>
-        <div>
-          <span>아이디</span>
-          <input />
-        </div>
-        <div>
-          <span>성별</span>
-          <div className={styles.radio_group}>
-            <label>
-              <input type="radio" name="gender" value="male" />
-              남자
-            </label>
-            <label>
-              <input type="radio" name="gender" value="female" />
-              여자
-            </label>
+        <form>
+          <div>
+            <span>아이디</span>
+            <input type="text" required />
           </div>
-        </div>
-        <div>
-          <span>전화번호</span>
-          <div className={styles.phone_number}>
-            <input />
-            <button>인증 요청</button>
+          <div>
+            <span>비밀번호</span>
+            <input type="password" required />
           </div>
-        </div>
-        <div className={styles.Certification_number}>
-          <span>인증번호</span>
-          <input></input>
-          <button>확인</button>
-        </div>
-        <div>
-          <span>주민등록번호</span>
-          <div className={styles.resident_registration_number}>
+          <div>
+            <span>아이디</span>
+            <input type="text" required />
+          </div>
+          <div>
+            <span>성별</span>
+            <div className={styles.radio_group}>
+              <label>
+                <input type="radio" name="gender" value="male" />
+                남자
+              </label>
+              <label>
+                <input type="radio" name="gender" value="female" />
+                여자
+              </label>
+            </div>
+          </div>
+          <div>
+            <span>전화번호</span>
+            <div className={styles.phone_number}>
+              <input />
+              <button>인증 요청</button>
+            </div>
+          </div>
+          {/* <div className={styles.Certification_number}> */}
+          <div className={styles.Certification_number}>
+            <span>인증번호</span>
             <input></input>
-            <span>-</span>
+            <button>확인</button>
+          </div>
+          <div>
+            <span>주민등록번호</span>
+            <div className={styles.resident_registration_number}>
+              <input></input>
+              <span>-</span>
+              <input></input>
+            </div>
+          </div>
+          <div>
+            <span>주소</span>
             <input></input>
           </div>
-        </div>
-        <div>
-          <span>주소</span>
-          <input></input>
-        </div>
-        <button className={styles.signup_button}>가입하기</button>
+          <button className={styles.signup_button}>가입하기</button>
+        </form>
       </div>
     </div>
   );
